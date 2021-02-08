c747a29aa155f42eb193f4497255b3ce92d0b225
PurpleFiddler <harrison@purplefiddler.co.uk>
2021-02-08 19:58:14 +0000

    REMOVED mute;ADDED autoID

--- blue-ref.sh
+++ blue-ref.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-btID=14:C1:4E:78:6A:CA
+btID=$(bluetoothctl info | grep Device | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')
 #VARIABLE ---> EXECUTE
-amixer set Master mute
 xdotool key XF86AudioPlay
 service bluetooth restart
 systemctl is-active --quiet bluetooth && echo Service has restarted
 while true; do #Inf. loop. Executes BT connect command via if statement.
    if [[ $(bluetoothctl connect $btID) == *"yes"* ]]; then
       echo "Connected"
-      amixer set Master unmute
       xdotool key XF86AudioPlay
       break #break will break out of the loop cycle
    else
